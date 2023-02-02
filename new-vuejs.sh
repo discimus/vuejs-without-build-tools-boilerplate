5cc3bf589a00af328b00d9375251f679323d475e
JonataPereira <jonata@financas360.com.br>
2023-02-02 19:27:01 -0300

    Revisar shell script

--- new-vuejs.sh
+++ new-vuejs.sh
@@ -1,15 +1,11 @@
 SOURCE_PATH="vuejs-empty-project"
 DESTINATION_PATH="vuejs-$1"
 
-echo "Copiando arquivos de '$SOURCE_PATH' to '$DESTINATION_PATH' ..."
 
 echo "Criando diretório '$DESTINATION_PATH' ..."
 mkdir $DESTINATION_PATH
 
-for path in ${SOURCE_PATH}/*
-do
-    filename=${path##*/}
-    cp $path "$DESTINATION_PATH/$filename"
-done
+echo "Copiando arquivos de '$SOURCE_PATH' para '$DESTINATION_PATH' ..."
+cp -a $SOURCE_PATH/. $DESTINATION_PATH
 
 echo "Arquivos copiados com sucesso!"
